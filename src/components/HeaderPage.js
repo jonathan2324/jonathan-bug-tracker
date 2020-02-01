ac1b2f0aab5cf1fa55f359cee2582d665a092b47
Jonathan Huertas <jonathanhuertas@Jonathans-MacBook-Pro-2.local>
2020-02-01 15:52:18 -0600

    Changed title of header

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -5,7 +5,7 @@ import {Link } from 'react-router-dom';
 const HeaderPage = () => {
   return (
     <div>
-      <h1>Bug Tracker</h1>
+      <h1>Task Manager</h1>
       <Link to="/">Dashboard</Link>
       <Link to="/create">Add Bug</Link>
     </div>
